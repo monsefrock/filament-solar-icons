45bb6fcde9e775410121fc5b6c10006897b8ff41
Monsef Eledrisse <zaone035@gmail.com>
2025-08-03 15:56:11 +0200

    feat: add SolarSet enum

---
+++ src/SolarSet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Monsefeledrisse\FilamentSolarIcons;
+
+enum SolarSet: string
+{
+    case Outline = 'solar-outline';
+    case Linear = 'solar-linear';
+    case Bold = 'solar-bold';
+    case BoldDuotone = 'solar-bold-duotone';
+    case Broken = 'solar-broken';
+    case LineDuotone = 'solar-line-duotone';
+
+    public static function all(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
